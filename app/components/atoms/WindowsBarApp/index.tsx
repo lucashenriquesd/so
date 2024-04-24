65348401341c12f392a8a960c1dcb0d93a18eb84
Lucas Henrique <lucashenriquesd@gmail.com>
2024-04-24 14:31:02 -0300

    added WindowsBarApp selection

--- app/components/atoms/WindowsBarApp/index.tsx
+++ app/components/atoms/WindowsBarApp/index.tsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+
 export default function WindowsBarApp() {
+  const [selected, setSelected] = useState(false);
+
+  function handleclick() {
+    console.log("My Computer clicked");
+    setSelected(!selected);
+  }
+
+  const containerClassNames = [
+    "m-2",
+    "min-w-[180px]",
+    "border-t",
+    "border-l",
+  ];
+
+  const pseudoClassNames = [
+    "flex",
+    "justify-start",
+    "border-b",
+    "border-r",
+  ];
+
+  if (selected) {
+    containerClassNames.push("border-gray-200");
+    pseudoClassNames.push("border-gray-500");
+  } else {
+    containerClassNames.push("border-gray-500");
+    pseudoClassNames.push("border-gray-200");
+  }
+
   return (
-    <div className="m-2 border-t border-l border-gray-200 min-w-[180px]">
-      <div className="flex justify-start border-b border-r border-gray-500">
-        <button className="p-[2px] flex-1 text-start" onClick={() => console.log('My Computer clicked')}>
+    <div className={containerClassNames.join(" ")}>
+      <div className={pseudoClassNames.join(" ")}>
+        <button className="p-[2px] flex-1 text-start" onClick={handleclick}>
           My Computer
         </button>
       </div>
