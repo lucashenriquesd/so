fe354ba280a87697b6098df72a38756f5a2ae8c5
Lucas Henrique <lucashenriquesd@gmail.com>
2024-04-24 22:09:28 -0300

    improved menu cursor

--- app/components/atoms/WindowsBarMenuItem/index.tsx
+++ app/components/atoms/WindowsBarMenuItem/index.tsx
@@ -7,7 +7,7 @@ type WindowsBarMenuItemProps = {
 export default function WindowsBarMenuItem(props: WindowsBarMenuItemProps) {
   return (
     <div className="flex hover:bg-[#000ab2] hover:text-white">
-      <button onClick={props.onClick} className="p-[2px] flex-1 text-start">{props.text}</button>
+      <button onClick={props.onClick} className="p-[2px] flex-1 text-start cursor-default">{props.text}</button>
     </div>
   );
 }
